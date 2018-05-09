41b36d4f5141865629906d79a8eeceaef6522852
Chengang <chengang@mokotechnology.com>
2018-05-09 16:58:48 +0800

    Initial Commit

---
+++ fitpolo705SDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fitpolo705SDKDemo
+//
+//  Created by aa on 2018/5/9.
+//  Copyright © 2018年 HCK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fitpolo705SDKDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fitpolo705SDKDemo
+//
+//  Created by aa on 2018/5/9.
+//  Copyright © 2018年 HCK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
